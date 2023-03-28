f19c6db26e077c2ec8df02cf98987f756b87616f
Alyxcq <Alyxcq@outlook.com>
2023-03-28 21:10:45 +0300

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int firstSales[] = {1, 2, 3, 4, 5};
+        SalesManager manager = new SalesManager(firstSales);
 
+        int x;
+        x = manager.max();
+        System.out.println(x);
     }
 }
